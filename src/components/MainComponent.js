161bfb01193688cba3c3a424f55a646c2478a2de
Pegas13fps <Tymoschenkob@gmail.com>
2019-06-23 23:58:45 +0300

    changed files

---
+++ src/components/MainComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Main() {
+    return (
+      <main className="main">
+        <input type="checkbox" />
+        <p>Placeholder must be here</p>
+        
+        <input type="checkbox" />
+        <p>Placeholder must be here</p>
+
+        <input type="checkbox" />
+        <p>Placeholder must be here</p>
+      </main>
+    )
+}
+
+export default Main;
